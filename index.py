79053fdf48a84defb000ab0dce205eb7afed4853
Adam Novotny <1@adamnovotny.com>
2016-02-01 21:47:58 -0500

    remove wiki json

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import os
-import requests
+#import requests
 from flask import Flask, render_template, request
-from flask import redirect, jsonify, json
+from flask import redirect
+#from flask import jsonify, json
 
 # variables
 app = Flask(__name__)
@@ -12,11 +13,11 @@ def show_index():
   return render_template('index.html')
 
 # return wiki API response
-@app.route('/wiki/<city>')
-def return_wiki_json(city):
-  wiki = requests.get('https://en.wikipedia.org/w/api.php?action=' + \
-                      'opensearch&search=' + city  + '&format=json').json()
-  return jsonify({'res': wiki[1]})
+#@app.route('/wiki/<city>')
+#def return_wiki_json(city):
+#  wiki = requests.get('https://en.wikipedia.org/w/api.php?action=' + \
+#                      'opensearch&search=' + city  + '&format=json').json()
+#  return jsonify({'res': wiki[1]})
 
 
 if __name__ == '__main__':
