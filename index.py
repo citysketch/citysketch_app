c3ab8493f0c9ed8a65804eee7ea8c8b12f61d640
Adam Novotny <1@adamnovotny.com>
2016-02-01 21:35:54 -0500

    add wiki json response /wiki/<city name>

--- index.py
+++ index.py
@@ -1,14 +1,23 @@
 import os
+import requests
 from flask import Flask, render_template, request
-from flask import redirect
+from flask import redirect, jsonify, json
 
 # variables
 app = Flask(__name__)
 
 # show index.html
-@app.route("/")
-def showIndex():
+@app.route('/')
+def show_index():
   return render_template('index.html')
+
+# return wiki API response
+@app.route('/wiki/<city>')
+def return_wiki_json(city):
+  wiki = requests.get('https://en.wikipedia.org/w/api.php?action=' + \
+                      'opensearch&search=' + city  + '&format=json').json()
+  return jsonify({'res': wiki[1]})
+
 
 if __name__ == '__main__':
   app.debug = True # used for dev only
