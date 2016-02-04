6474944ff0a93c2a1c67594dfe02becde19a7e83
Adam Novotny <1@adamnovotny.com>
2016-02-03 22:02:41 -0500

    Add wiki bootstrap accordion

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,35 @@
 
-// loadData updates all city-specific page areas
-var loadData = function() {
-    var $city = $('#city-input').val();
-    
-    // TO DO streetview
+// updateWiki updates #wiki-accordion with wiki search articles
+var updateWiki = function(city) {
+    var $city = $('#city-input').val().toUpperCase();
+    // TO DO AJAX -------------------------
 
-    // TO DO wiki
+    // TO DO ---------------- udpate AJAX contents
+    $('#wiki-accordion').empty();
+    for (i = 0; i < 3; i++) {
+	$title = $city + " title article " + i;
+	$contents = $city + " article contents " + i;
+	$collapse = (i < 2) ? " in" : "";
+	$('#wiki-accordion').append('<div class="panel panel-primary">' +
+				    '<div class="panel-heading">' +
+				    '<h4 class="panel-title">' +
+				    '<a data-toggle="collapse" data-parent="#accordion"' + 
+				    'href="#collapse' + i + '">' +
+				    $title + '</a></h4></div>' +
+				    '<div id="collapse' + i + 
+				    '" class="panel-collapse collapse' + $collapse  + '">' +
+				    '<div class="panel-body"><p>' +
+				    $contents + '</p></div></div></div>');
+
+    }
     alert("City searched: " + $city);
-    // END wiki
-
-    return false;
+    //END wiki
 };
 
-
 // run on load of site
-$(function() {
-    alert("Expected launch:  March 1, 2016");
-    $('#search-form').submit(loadData);
+$(function() {    
+    
+    $('#search-button').on('click', function() {
+	updateWiki();
+    });
 });
